c87d36d6c2b787e7b75c399a843c07d91346035d
Ekta Khanna <ekhanna@pivotal.io>
2017-05-09 16:32:20 -0700

    Remove InClauseInfo and OuterJoinInfo
    Since `InClauseInfo` and `OuterJoinInfo` are now combined into
    `SpecialJoinInfo`; remove them from the relevant places.
    
    Access `join_info_list` instead of `in_info_list` and `oj_info_list`
    
    Previously, `CdbRelDedupInfo` contained list of `InClauseInfo` s. While
    making join decisions and overall join processing, we traversed this list
    and invoked cdb specific functions: `cdb_make_rel_dedup_info()`, `cdbpath_dedup_fixup()`
    
    Since `InClauseInfo` is no longer available,  `CdbRelDedupInfo` will contain list of
    `SpecialJoinInfo` s. All the cdb specific routines which were previously called for
    `InClauseInfo` list will now be called if `CdbRelDedupInfo` has valid `SpecialJoinInfo`
    list and if join type in `SpecialJoinInfo` is `JOIN_SEMI`. A new helper routine `hasSemiJoin()`
    has been added which traverses `SpecialJoinInfo` list to check if it contains `JOIN_SEMI`.
    
    Ref [#142355175]
    
    Signed-off-by: Dhanashree Kashid <dkashid@pivotal.io>

--- src/backend/optimizer/util/clauses.c
+++ src/backend/optimizer/util/clauses.c
@@ -5040,11 +5040,10 @@ flatten_join_alias_var_optimizer(Query *query, int queryLevel)
 	root->init_plans = NIL;
 
 	root->list_cteplaninfo = NIL;
-	root->in_info_list = NIL;
+	root->join_info_list = NIL;
 	root->append_rel_list = NIL;
 
 	root->hasJoinRTEs = false;
-	root->hasOuterJoins = false;
 
 	ListCell *plc = NULL;
 	foreach(plc, queryNew->rtable)
@@ -5056,7 +5055,6 @@ flatten_join_alias_var_optimizer(Query *query, int queryLevel)
 			root->hasJoinRTEs = true;
 			if (IS_OUTER_JOIN(rte->jointype))
 			{
-				root->hasOuterJoins = true;
 				break;
 			}
 		}
